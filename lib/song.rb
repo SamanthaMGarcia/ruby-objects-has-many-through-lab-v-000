7ea8fbc8e999169174ca24cc3148a36f66d87d82
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-21 17:09:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,9 @@
 class Song
-  attr_accessor :name, :genre
-  
+  attr_accessor :name, :artist, :genre
+
   def initialize(name, genre)
     @name = name
     @genre = genre
-  end
-  
-  def add_song(song)
-    @songs << song
-    song.artist = self
+    genre.add_song(self)
   end
 end
