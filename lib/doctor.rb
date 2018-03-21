8cd0f4f12dfa924a8d8a2869a3768f367eeade78
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-21 17:45:56 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,7 +16,7 @@ class Doctor
   end
   
   def patients
-    self.patients.collect {|patient| appointment.patient}
+    self.appointments.collect {|appointment| appointment.patient}
   end
 end
     
